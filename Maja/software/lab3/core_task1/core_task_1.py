992e5060669f9c0e7a2dc630f5ab0759b53aa8bb
majaPettersson5-boop <majaPettersson5@icloud.com>
2026-05-05 13:25:29 +0200

    the list loop for core task 1 in lab 3

---
+++ Maja/software/lab3/core_task1/core_task_1.py
@@ -0,0 +1,6 @@
+# Lista med aminosyror
+amino_acids = ["Methionine", "Leucine", "Glycine", "Alanine"]
+
+# En loop som går igenom listan och skriver ut varje namn
+for aa in amino_acids:
+    print(f"Processing amino acid: {aa}")
